fb4cb900207a3988e4c2fb7b7bfcc52eb39bd977
Esra <esra.hofstede@topicus.nl>
2023-03-03 10:43:34 +0100

    Dotnet 7

---
+++ src/DotnetWebapplication/Controllers/DemoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetWebapplication.Controllers
+{
+    [Route("/api/[controller]")]
+    public class DemoController : Controller
+    {
+        public OkObjectResult Index()
+        {
+            return Ok("Test");
+        }
+    }
+}
